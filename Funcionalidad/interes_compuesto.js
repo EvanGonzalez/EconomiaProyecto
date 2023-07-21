aca0784bdc330af7b811642c3a2cae9082ba88b8
nicoalex81 <53489833+nicoalex81@users.noreply.github.com>
2023-07-20 21:05:54 -0400

    Update interes_compuesto.js

---
+++ Funcionalidad/interes_compuesto.js
@@ -0,0 +1,28 @@
+function calcularMontoCompuesto(C, I) {
+  return C + I;
+}
+
+function calcularInteresCompuesto(S, C) {
+  return S - C;
+}
+
+function calcularCapitalCompuesto(S, i, n) {
+  return S / Math.pow(1 + i, n);
+}
+
+// Ejemplo de uso
+const C = 1000; // Capital inicial
+const i = 0.05; // Tasa de interés (5%)
+const n = 12;   // Periodo en meses (puede ser cualquier otro valor para diferentes periodos)
+
+// Calcular Monto Compuesto
+const montoCompuesto = calcularMontoCompuesto(C, C * i * n);
+console.log("Monto compuesto:", montoCompuesto);
+
+// Calcular Interés Compuesto
+const interesCompuesto = calcularInteresCompuesto(montoCompuesto, C);
+console.log("Interés compuesto:", interesCompuesto);
+
+// Calcular Capital Compuesto
+const capitalCompuesto = calcularCapitalCompuesto(montoCompuesto, i, n);
+console.log("Capital compuesto:", capitalCompuesto);
