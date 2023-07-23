327fa516b1d77d1370ed229f763b83c70cdc9729
nicoalex81 <53489833+nicoalex81@users.noreply.github.com>
2023-07-22 21:19:21 -0400

    Update Funcionalidad.js

--- Funcionalidad/Funcionalidad.js
+++ Funcionalidad/Funcionalidad.js
@@ -18,7 +18,7 @@ function calcularMontoCompuesto2(C, I) {
 }
 
 function calcularInteresCompuesto(S, C) {
-    if (S <= 0 || C <= 0 || S <= C) {
+    if (S <= 0 || C <= 0) {
         return "Error, los datos no pueden ser menores a 1"
     } else if (C <= S) {
         return "Error, el capital no puede ser mayor que el monto"
