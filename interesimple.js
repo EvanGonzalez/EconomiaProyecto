b6a02c0fac88a8e4fce5fe12b712d91146d7ace3
sora1 <gonzalezevans05@gmail.com>
2023-07-23 13:04:37 -0500

    asilar botones de interes simple 1:04 pm

--- interesimple.js
+++ interesimple.js
@@ -1,51 +1,31 @@
 
-
-
-
-function convertToDecimal() {
-  var integerInput = document.getElementById('integerInput');
-  var decimalInput = document.getElementById('decimalInput');
-
-  var integerValue = parseInt(integerInput.value);
-  var decimalValue = integerValue / 100; // Dividir por 100 para convertir en decimal
-
-  decimalInput.value = decimalValue.toFixed(2); // Limitar a 2 decimales
-}
-//  para desaparecer botones 
-function handleButtonClick(button) {
-    // Obtener todos los botones con la clase "boton-controlable"
-    const buttons = document.getElementsByClassName('boton-controlable');
-
-    // Deshabilitar todos los botones excepto el botón clickeado
-    for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i] !== button) {
-            buttons[i].disabled = true;
-        }
-    }
-
-    // Volver a habilitar los botones después de un tiempo (puedes ajustar el tiempo según tus necesidades)
-    setTimeout(function() {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].disabled = false;
-        }
-    }, 3000); // Aquí, los botones se habilitarán después de 3 segundos (3000 milisegundos)
-}
-
-// fin de  desaparecer botones 
-
-
 function showInput(inputId) {
-  var input = document.getElementById(inputId);
-  var buttons = document.getElementsByClassName('button');
+    var input = document.getElementById(inputId);
+    var buttons = document.getElementsByClassName('button');
 
-  if (input.style.display === "none") {
-    input.style.display = "block";
-    disableOtherButtons(inputId, buttons);
-  } else {
-    input.style.display = "none";
-    enableAllButtons(buttons);
-  }
+    if (input.style.display === "none") {
+        input.style.display = "block";
+        disableOtherButtons(inputId, buttons);
+    } else {
+        input.style.display = "none";
+        enableAllButtons(buttons);
+    }
 }
+
+function disableOtherButtons(selectedInputId, buttons) {
+    for (var i = 0; i < buttons.length; i++) {
+      var button = buttons[i];
+      if (button.id !== "button" + selectedInputId.slice(5)) {
+        button.disabled = true;
+      }
+    }
+  }
+  
+  function enableAllButtons(buttons) {
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = false;
+    }
+  }
 
 
 
@@ -57,160 +37,160 @@ const resultTiempo = document.querySelector("#resultcalTiempo");
 
 
 
-function calCapital1(){         
+function calCapital1() {
     //variable monto
-    let monto = document.getElementById('montoC1').value; 
+    let monto = document.getElementById('montoC1').value;
     //variable interes
     let interes = document.getElementById('interesC1').value;
-    
+
     //Calculo del capital 1
-    let capital1 = ( monto - interes); 
-    
+    let capital1 = (monto - interes);
+
     //alerta para confirmar datos
     var resConfirmacion = confirm("¿Deseas enviar esta información?");
-    
-    if (resConfirmacion == true){
-        
+
+    if (resConfirmacion == true) {
+
         //si acepta entonces valida que no sean negativos, ni estén vacíos
-        if(monto >0 && interes >0 && monto != null && interes != null){
-                //retorna resultado
-                return resultCapital1.innerHTML = "El resultado es:  C = "+ monto + ' - ' + interes + ' = ' + capital1.toFixed(2);
-            }else{
-                //return resultCapital1.innerHTML = "Error. Hay datos negativos o campos vacíos.";
-                //alerta de error
-                var resAlert = window.alert("Error. Hay datos negativos o campos vacíos.");
-                return resultCapital1.innerHTML = "";
-            }
-    }else{
+        if (monto > 0 && interes > 0 && monto != null && interes != null) {
+            //retorna resultado
+            return resultCapital1.innerHTML = "El resultado es:  C = " + monto + ' - ' + interes + ' = ' + capital1.toFixed(2);
+        } else {
+            //return resultCapital1.innerHTML = "Error. Hay datos negativos o campos vacíos.";
+            //alerta de error
+            var resAlert = window.alert("Error. Hay datos negativos o campos vacíos.");
+            return resultCapital1.innerHTML = "";
+        }
+    } else {
         return false;
     }
 }
 
-function calCapital2(){         
+function calCapital2() {
     //varibale interes
-    let interes = document.getElementById('interesC2').value; 
+    let interes = document.getElementById('interesC2').value;
     //variable tasa de interes
     let tasa = document.getElementById('tasainteresC2').value;
-    var tasainteres = tasa /100;
+    var tasainteres = tasa / 100;
     //variable tiempo
     let tiempo = document.getElementById('tiempoC2').value;
-    
+
     //capta el valor los radio
     let elementoActivo = document.querySelector('input[name="tipotiempo"]:checked').value;
-    
+
     //alerta de confirmacion de datos
     var resConfirmacion = confirm("¿Deseas enviar esta información?");
-    
-    if (resConfirmacion == true){
-        
+
+    if (resConfirmacion == true) {
+
         //si acepta entonces valida que no sean negativos, ni estén vacíos
-        if(tasainteres >0 && interes >0 && tiempo >0 && tasainteres != null && interes != null && tiempo != null){
-            
+        if (tasainteres > 0 && interes > 0 && tiempo > 0 && tasainteres != null && interes != null && tiempo != null) {
+
             //si la tasa de interes es anual y el tiempo es en años ó si la tasa es mensual y el tiempo es en meses
-            if(elementoActivo == "Anos"){
+            if (elementoActivo == "Anos") {
 
                 //calculo del capital
                 let capital2 = interes / (tasainteres * tiempo);
 
                 //retorna resultado
-                return resultCapital2.innerHTML = "El resultado es:  C = "+ interes + " / " + tasainteres + " * " + tiempo + " = " + capital2.toFixed(2);
+                return resultCapital2.innerHTML = "El resultado es:  C = " + interes + " / " + tasainteres + " * " + tiempo + " = " + capital2.toFixed(2);
 
-            }else{
+            } else {
                 //sino si la tasa es anual y el tiempo es en meses, se divide el tiempo entre 12
-                if(elementoActivo == "Meses"){
-                    
+                if (elementoActivo == "Meses") {
+
                     //calculo del capital
-                    let capital2 = interes / (tasainteres * (tiempo/12));
+                    let capital2 = interes / (tasainteres * (tiempo / 12));
 
                     //retorna resultado
-                    return resultCapital2.innerHTML = "El resultado es:  C = "+ interes + " / ( " + tasainteres + " * " + tiempo + " / 12 ) " + " = " + capital2.toFixed(2);
-                
-                }else{
+                    return resultCapital2.innerHTML = "El resultado es:  C = " + interes + " / ( " + tasainteres + " * " + tiempo + " / 12 ) " + " = " + capital2.toFixed(2);
+
+                } else {
                     //si no se selecciona nada en los radio
-                    if(elementoActivo == "" ){
+                    if (elementoActivo == "") {
                         //alerta de error
                         var resAlert = window.alert("Error. No selecciono una opción.");
                         return resultCapital2.innerHTML = "";
                     }
                 }
             }
-        }else{
+        } else {
             //return resultCapital1.innerHTML = "Error. Hay datos negativos o campos vacíos.";
             //alerta de erro si hay negativos o campos vacios.
             var resAlert = window.alert("Error. Hay datos negativos o campos vacíos.");
             return resultCapital2.innerHTML = "";
         }
-        
-    }else{
+
+    } else {
         //retorna falso si no acepta enviar los datos.
         return false;
     }
 }
 
-function calCapital3(){         
+function calCapital3() {
     //variable 1
-    let monto = document.getElementById('montoC3').value; 
+    let monto = document.getElementById('montoC3').value;
     //variable 2
     let tasa = document.getElementById('tasainteresC3').value;
     //convierte a decimal la tasa
-    var tasainteres = tasa /100;
+    var tasainteres = tasa / 100;
     //variable 3
-    let tiempo = document.getElementById('tiempoC3').value; 
-    
+    let tiempo = document.getElementById('tiempoC3').value;
+
     //calculo de capital 3
-    let capital3 = monto / (1 + (tasainteres * tiempo)); 
-    
-    
+    let capital3 = monto / (1 + (tasainteres * tiempo));
+
+
     //alerta para confirmar datos
     var resConfirmacion = confirm("¿Deseas enviar esta información?");
-    
-    if (resConfirmacion == true){
+
+    if (resConfirmacion == true) {
         //si acepta entonces valida que no sean negativos, ni estén vacíos
-        if(tasainteres >0 && monto >0 && tiempo >0 && tasainteres != null && monto != null && tiempo != null){
-                
-                //retorna el resultado
-                return resultCapital3.innerHTML = "El resultado es:  C = "+ monto + " / (1 + (" + tasainteres + " * " + tiempo + "))" + " = " + capital3.toFixed(2);
-            }else{
-                
-                //return resultCapital1.innerHTML = "Error. Hay datos negativos o campos vacíos.";
-                //Alerta de error
-                var resAlert = window.alert("Error. Hay datos negativos o campos vacíos.");
-                return resultCapital3.innerHTML = "";
-            }
-    }else{
+        if (tasainteres > 0 && monto > 0 && tiempo > 0 && tasainteres != null && monto != null && tiempo != null) {
+
+            //retorna el resultado
+            return resultCapital3.innerHTML = "El resultado es:  C = " + monto + " / (1 + (" + tasainteres + " * " + tiempo + "))" + " = " + capital3.toFixed(2);
+        } else {
+
+            //return resultCapital1.innerHTML = "Error. Hay datos negativos o campos vacíos.";
+            //Alerta de error
+            var resAlert = window.alert("Error. Hay datos negativos o campos vacíos.");
+            return resultCapital3.innerHTML = "";
+        }
+    } else {
         return false;
     }
 }
 
-function calTiempo(){
+function calTiempo() {
     //variable interes
-    let interes = document.getElementById('interesT').value; 
+    let interes = document.getElementById('interesT').value;
     //variable capital
     let capital = document.getElementById('capitalT').value;
     //variable tasa de interes
-    let tasa = document.getElementById('tasainteresT').value; 
-    var tasainteres = tasa /100;
-    
+    let tasa = document.getElementById('tasainteresT').value;
+    var tasainteres = tasa / 100;
+
     //Calculo de tiempo
-    let tiem = interes / (capital * tasainteres); 
+    let tiem = interes / (capital * tasainteres);
     let tiempo = tiem * 12;
-    
+
     //alerta para confirmar datos
     var resConfirmacion = confirm("¿Deseas enviar esta información?");
-    
-    if (resConfirmacion == true){
-        
+
+    if (resConfirmacion == true) {
+
         //si acepta entonces valida que no sean negativos, ni estén vacíos
-        if(interes >0 && capital >0 && tasainteres >0 && interes != null && capital != null && tasainteres != null){
-                //retorna el resultado
-                return resultTiempo.innerHTML = "El tiempo es:  t = "+interes+" / "+capital +" * "+tasainteres+" = "+ tiempo + " meses. ";
-            }else{
-                //return resultCapital1.innerHTML = "Error. Hay datos negativos o campos vacíos.";
-                //Alerta de error
-                var resAlert = window.alert("Error. Hay datos negativos o campos vacíos.");
-                return resultCapital3.innerHTML = "";
-            }
-    }else{
+        if (interes > 0 && capital > 0 && tasainteres > 0 && interes != null && capital != null && tasainteres != null) {
+            //retorna el resultado
+            return resultTiempo.innerHTML = "El tiempo es:  t = " + interes + " / " + capital + " * " + tasainteres + " = " + tiempo + " meses. ";
+        } else {
+            //return resultCapital1.innerHTML = "Error. Hay datos negativos o campos vacíos.";
+            //Alerta de error
+            var resAlert = window.alert("Error. Hay datos negativos o campos vacíos.");
+            return resultCapital3.innerHTML = "";
+        }
+    } else {
         return false;
     }
 }
